050c631cbd537d0872684897df1456fbc4226122
helvnor <henrik@elvinnorrman.com>
2026-03-16 15:43:18 +0100

    fix(hypr): set to dark mode

--- modules/nixos/hypr.nix
+++ modules/nixos/hypr.nix
@@ -6,10 +6,22 @@
     hypridle
     hyprpaper
 
-    # Tools 
+    # Tools
     nwg-displays
     nautilus
   ];
+
+  # Dark mode
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+  };
+
+  gtk = {
+    enable = true;
+    gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
+  };
 
   home.file.".config/hypr" = {
     source = builtins.path {
