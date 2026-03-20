d25b68695135135bd6e9908ecd12db59ec0529df
helvnor <henrik@elvinnorrman.com>
2026-03-20 12:16:00 +0100

    fix(zenbook): fix noctalia icons

--- hosts/zenbook/home.nix
+++ hosts/zenbook/home.nix
@@ -22,8 +22,16 @@
     };
   };
 
+  home.sessionVariables = {
+    QS_ICON_THEME = "Papirus-Dark";
+  };
+
   gtk = {
     enable = true;
+    iconTheme = {
+      package = pkgs.papirus-icon-theme;
+      name = "Papirus-Dark";
+    };
     gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
   };
 
